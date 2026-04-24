2f31e328a658cfe9e2dcce5ab4d09343aa6f6657
Sandesh Kashyap <155136430+SandeshK17@users.noreply.github.com>
2026-04-24 23:15:37 +0530

    Add comment to main method in factorial.java
    Added comment explaining the factorial calculation.

--- factorial.java
+++ factorial.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 public class factorial {
-    public static void main(String[] args) {
+    public static void main(String[] args) { // Factorial of a number is product of number from 1 to that number.
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number: ");
         int number = sc.nextInt();
