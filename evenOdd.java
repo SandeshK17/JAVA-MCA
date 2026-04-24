4b7c0dc8877d81243f183a8710e07be67571d904
Sandesh Kashyap <155136430+SandeshK17@users.noreply.github.com>
2026-04-24 23:18:43 +0530

    Add comments for clarity in evenOdd.java
    Added comments to clarify even and odd number checks.

--- evenOdd.java
+++ evenOdd.java
@@ -5,10 +5,10 @@ class evenOdd {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter number to be checked as even or odd:");
         num = sc.nextInt();
-        if(num % 2 == 0) {
+        if(num % 2 == 0) { // If a number is even the remainder is zero
             System.out.println(num + " is an Even number.");
         }
-        else{
+        else{ // If the remainder is not 0 number is odd
             System.out.println(num + " is an Odd number.");
         }
         sc.close();
